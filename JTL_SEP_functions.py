e126e157a37f33678361302a02c737c9eb652aab
Jax Lang <62795415+JaxLang@users.noreply.github.com>
2025-11-04 11:12:25 +0200

    Foundational files
    Setting up a basic repo, will tidy later.

---
+++ JTL_SEP_functions.py
@@ -0,0 +1,31 @@
+import os
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+from scipy.optimize import curve_fit
+from scipy import odr
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+
+from sunpy.time import parse_time
+import astropy.constants as aconst
+import astropy.units as u
+
+## Solar mach
+# 1. initial plot to determine if event is good
+# 2. loop to get sc locations over time
+
+
+## Data loaders
+
+
+## Inter-calibration
+
+
+## Radial Scaling
+
+
+## Gaussian fitting
