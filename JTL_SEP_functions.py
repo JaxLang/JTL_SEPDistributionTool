ca5deed21d2c2ee2609fe4903d30bd036b1a26f9
Jax Lang <62795415+JaxLang@users.noreply.github.com>
2025-11-05 15:28:11 +0200

    Fixed the df that returns to the user file
    Trying to add the solarmach values but the function has gone down at some point today (something about cda function not included in SOHO loader). Once the solarmach values are added to the full df, the next functions should be included seamlessly.

--- JTL_SEP_functions.py
+++ JTL_SEP_functions.py
@@ -19,10 +19,162 @@ from seppy.loader.soho import soho_load
 from seppy.loader.stereo import stereo_load
 from solo_epd_loader import epd_load
 
+from solarmach import SolarMACH
+
 ## Solar mach
 # 1. initial plot to determine if event is good
-# 2. loop to get sc locations over time
+def solarmach_basic(startdate, data_path, coord_sys='Stonyhurst', source_location=None):
+    filename = f'SolarMACH_{startdate.strftime("%d%m%Y_%H:%M")}'
+    print(filename)
+    print(os.listdir(data_path))
+    if filename+'.csv' in os.listdir(data_path):
+        print('WIN')
+        smtable = pd.read_csv(data_path+filename+'.csv', index_col='Spacecraft/Body')
+        return smtable
+        
+    datetime_parse = parse_time(startdate)
 
+    observers = ['Earth', 'PSP', 'Solar Orbiter', 'STEREO-A', 'BepiColombo']
+
+    sm1 = SolarMACH(datetime_parse, observers, vsw_list=[], 
+                    reference_long=source_location[0],  reference_lat=source_location[1], 
+                    coord_sys='Stonyhurst')
+
+    sm1_tab = sm1.coord_table.copy()
+    sm1_tab.index = sm1_tab['Spacecraft/Body']
+    sm1_tab.to_csv(f'{data_path+filename}.csv')
+
+    # Show the polar plot
+    sm1.plot(plot_spirals=True, plot_sun_body_line=True, reference_vsw=400,
+             transparent=False, numbered_markers=False, long_offset=270, 
+             return_plot_object=False, 
+             outfile=f'{data_path+filename}.png')
+
+    return sm1
+
+    
+# 2. loop to get sc locations over time
+def solarmach_loop(observers, dates, data_path, source_loc=[None,None], coord_sys='Stonyhurst'):
+    filename = f'{data_path}/SolarMACH_{dates[0].strftime("%d%m%Y")}_loop.csv'
+
+    if filename in os.listdir(data_path):
+        sm_loop = pd.read_csv(filename, index_col='Time')
+        return sm_loop
+
+    # Set up the time list to iterate through
+    starting_time = dt.datetime(dates[0].year, dates[0].month, dates[0].day, dates[0].hour, 0)
+    date_list = pd.date_range(starting_time, dates[1], freq='15min')
+
+    sm_df = {}
+    for t in date_list:
+        sm10 = SolarMACH(t, observers, vsw_list=[], 
+                         reference_long=source_loc[0], 
+                         reference_lat=source_loc[1], 
+                         coord_sys='Stonyhurst')
+        sm_df[t] = sm10.coord_table
+
+    # Create a new dict with each spacecraft df saved separately
+    df1 = {}
+    for obs in observers:
+        
+        r_dist, vsw, foot_long, foot_long_error = ([] for i in range(4))
+        for tt in date_list:
+            tmp_df = pd.DataFrame.from_dict(sm_df[tt])
+            tmp_df.set_index('Spacecraft/Body', inplace=True)
+
+            r_dist.append( tmp_df['Heliocentric distance (AU)'][obs] )
+            vsw.append( tmp_df['Vsw'][obs] )
+
+            
+
+            foot_calc = move_along_parker_spiral(
+                r_dist=tmp_df['Heliocentric distance (AU)'][obs], 
+                loc=[float(tmp_df['Stonyhurst longitude (°)'][obs]), 
+                          float(tmp_df['Stonyhurst latitude (°)'][obs])],
+                vsw=tmp_df['Vsw'][obs], towards=True, err_calc=True)
+
+            foot_long.append( tmp_df['Magnetic footpoint longitude (Stonyhurst)'][obs] )
+            #print('Calculated foot: ', foot_calc)
+            #print('Given foot: ', tmp_df['Magnetic footpoint longitude (Stonyhurst)'][obs])
+            #jax=input('How are the feet? ')
+            foot_long_error.append( foot_calc[1] )
+
+        df1[obs] = {}
+        df1[obs]['r dist'] = r_dist
+        df1[obs]['vsw'] = vsw
+        df1[obs]['foot_long'] = foot_long
+        df1[obs]['foot_long_error'] = foot_long_error
+
+        
+    df2 = pd.DataFrame({(outer_key, inner_key): values
+                        for outer_key, inner_dict in df1.items()
+                        for inner_key, values in inner_dict.items()})
+
+    df2.index = date_list
+    df2.index.name = 'Time'
+
+
+    # Save to csv
+    df2.to_csv(filename) 
+
+    return df2
+    
+def move_along_parker_spiral(r_dist, loc, vsw, towards, err_calc):
+    #radial distance, location  [WE, NS],  measured Vsw, towards the Sun (True) or away (False), err_calc = True/False
+    sun_radius = aconst.R_sun.to(u.AU).value
+    if loc[0] < 0:
+        loc[0] = loc[0] + 360
+
+    # Calculating the solar wind parameters
+    omega_ref = np.radians((14.5 - 2.87 * (np.sin(np.deg2rad(loc[1]))**2)) / (24*60*60)) # in rad/s
+    if err_calc:
+        omega_vsw = []
+        new_loc_arr = []
+        for Vsw in [vsw, vsw-50, vsw+50]:
+            omega_vsw.append(omega_ref / (Vsw / 1.5e8)) # in rad/au
+    else:
+        omega_vsw = omega_ref / (vsw / 1.5e8) # in rad/au
+
+    # Calulcating the new location
+    if err_calc:
+        for Ov in omega_vsw:
+            if towards:
+                newloc_tmp = np.radians(loc[0]) - Ov * (sun_radius - r_dist) * ( np.cos( np.deg2rad( loc[1] ) ) )
+            else:
+                newloc_tmp = np.radians(loc[0]) + Ov * (sun_radius - r_dist) * ( np.cos( np.deg2rad( loc[1] ) ) )
+
+            newloc_t = float(np.degrees(newloc_tmp))
+
+            # Make sure it falls within the Stonyhurst restrictions
+            if newloc_t > 180:
+                newloc_t = newloc_t - 360
+            elif newloc_t < -180:
+                newloc_t = newloc_t + 360
+
+            # Add to the array
+            new_loc_arr.append(newloc_t)
+
+        # Find the nominal and uncertainty values
+        new_loc_unc = np.std(new_loc_arr, mean=new_loc_arr[0])
+        new_loc = [new_loc_arr[0], float(new_loc_unc)]
+    else:
+        if towards:
+            newloc_tmp = np.radians(loc[0]) - omega_vsw * (sun_radius - r_dist) * ( np.cos( np.deg2rad( loc[1] ) ) )
+        else:
+            newloc_tmp = np.radians(loc[0]) + omega_vsw * (sun_radius - r_dist) * ( np.cos( np.deg2rad( loc[1] ) ) )
+
+        new_loc = float(np.degrees(newloc_tmp))
+
+        # Make sure it falls within the Stonyhurst restrictions
+        if new_loc > 180:
+            new_loc = new_loc - 360
+        elif new_loc < -180:
+            new_loc = new_loc + 360
+
+
+    return new_loc
+    
+    
 ################################################
 ## Data loaders
 ################################################
@@ -57,17 +209,21 @@ def weighted_bin_merge(df0, spacecraft, species, channel_list, header_label, bin
 
 ################################################
 
-def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration=True, radial_scaling=True, resampling='15min'):
+def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration=True, radial_scaling=True, resampling='15min', reference_loc=[None,None]):
     """Load the data, merge the bins, make omnidirectional, resample, intercalibrate, radial scale, and return one df:
         -index: times
         - header1: sc-ins
         - [Flux, Uncertainty, Radial Distance, Longitude]"""
 
-    spacecraft = [x.lower() for x in spacecraft]
+    # Download solarmach data for the same intervals
+    sm_df = solarmach_loop(spacecraft, dates, data_path, source_loc=reference_loc, coord_sys='Stonyhurst')
+    print(sm_df)
+    jax=input('Continue?')
 
-
+    
     # Download the data and load into a dictionary with the key as the spacecraft-instrument label
     sc_dict = {}
+    spacecraft = [x.lower() for x in spacecraft]
 
     if 'psp' in spacecraft:
         psp_df, psp_meta = psp_isois_load(dataset='PSP_ISOIS-EPIHI_L2-HET-RATES60', # 'A_H_Flux_n' 'B_H_Uncertainty_n'
@@ -105,14 +261,14 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
         for tt in psp_df.index:
             flux_arr.append( np.nanmean([psp_df.loc[tt, f"A_F_{bin_label}"], psp_df.loc[tt, f"B_F_{bin_label}"]]) )
             unc_arr.append( np.nanmean([psp_df.loc[tt, f"A_Func_{bin_label}"], psp_df.loc[tt, f"B_Func_{bin_label}"]]) )
-        psp_df1["Flux"] = flux_arr
-        psp_df1["Uncertainty"] = unc_arr
+        psp_df1["PSP--Flux"] = flux_arr
+        psp_df1["PSP--Uncertainty"] = unc_arr
 
         psp_df2 = pd.DataFrame.from_dict(psp_df1)
         psp_df2.set_index('Time', inplace=True)
 
         # Resample
-        psp = psp_df2.resample(resample).mean()
+        psp = psp_df2.resample(resampling).mean()
 
 
         # Add to the collection
@@ -122,7 +278,7 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
 
     if 'soho' in spacecraft:
         soho_df, soho_meta = soho_load(dataset='SOHO_ERNE-HED_L2-1MIN', # 'PH_n' 'PHC_n'
-                                       startdate=startdate, enddate=enddate,
+                                       startdate=dates[0], enddate=dates[1],
                                        path=data_path+'soho/', resample='1min',
                                        pos_timestamp='start')
 
@@ -148,14 +304,14 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
 
         # Merge the channels
         soho_df1 = {'Time': soho_df.index}
-        soho_df1["Flux"] = weighted_bin_merge(soho_df, 'soho', 'p', bin_list, 'PH_', bin_width)
-        soho_df1["Uncertainty"] = weighted_bin_merge(soho_df, 'soho', 'p', bin_list, 'Uncert_', bin_width)
+        soho_df1["SOHO--Flux"] = weighted_bin_merge(soho_df, 'soho', 'p', bin_list, 'PH_', bin_width)
+        soho_df1["SOHO--Uncertainty"] = weighted_bin_merge(soho_df, 'soho', 'p', bin_list, 'Uncert_', bin_width)
 
         soho_df2 = pd.DataFrame.from_dict(soho_df1)
         soho_df2.set_index('Time', inplace=True)
 
         # Resample
-        soho = soho_df2.resample(resample).mean()
+        soho = soho_df2.resample(resampling).mean()
 
 
         # Add to the collection
@@ -163,7 +319,7 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
 
     if 'stereo-a' in spacecraft:
         sta_df, sta_meta = stereo_load(instrument='HET', spacecraft='ahead', # 'Proton_Flux_n' 'Proton_Sigma_n'
-                                       startdate=startdate, enddate=enddate,
+                                       startdate=dates[0], enddate=dates[1],
                                        path=data_path+'stereo/', resample='1min',
                                        pos_timestamp='start')
 
@@ -184,14 +340,14 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
 
         # Merge the channels
         sta_df1 = {'Time': sta_df.index}
-        sta_df1["Flux"] = weighted_bin_merge(sta_df, 'sta', 'p', bin_list, 'Proton_Flux_', bin_width)
-        sta_df1["Uncertainty"] = weighted_bin_merge(sta_df, 'sta', 'p', bin_list, 'Proton_Sigma_', bin_width)
+        sta_df1["STA--Flux"] = weighted_bin_merge(sta_df, 'sta', 'p', bin_list, 'Proton_Flux_', bin_width)
+        sta_df1["STA--Uncertainty"] = weighted_bin_merge(sta_df, 'sta', 'p', bin_list, 'Proton_Sigma_', bin_width)
 
         sta_df2 = pd.DataFrame.from_dict(sta_df1)
         sta_df2.set_index('Time', inplace=True)
 
         # Resample
-        sta = sta_df2.resample(resample).mean()
+        sta = sta_df2.resample(resampling).mean()
 
         # Add to the collection
         sc_dict['STEREO-A HET'] = sta
@@ -199,7 +355,7 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
 
     if 'solar orbiter' in spacecraft:
         solo_dfp, solo_dfe, solo_meta = epd_load(sensor='het', level='l2', # [('H_Flux','H_Flux_n')] [('H_Uncertainty','H_Uncertainty_n')]
-                                      startdate=startdate, enddate=enddate,
+                                      startdate=dates[0], enddate=dates[1],
                                       viewing='omni', autodownload=True,
                                       pos_timestamp='start', path=data_path+'solo/')
 
@@ -217,20 +373,22 @@ def load_sc_data(spacecraft, proton_channels, dates, data_path, intercalibration
 
         # Merge the channels
         solo_df2 = {'Time': solo_df1.index}
-        solo_df2['Flux'] = weighted_bin_merge(solo_df1, 'solo', 'p', bin_list, [('H_Flux','H_Flux_')], bin_width)
-        solo_df2["Uncertainty"] = weighted_bin_merge(solo_df1, 'solo', 'p', bin_list, [('H_Uncertainty','H_Uncertainty_')], bin_width)
+        solo_df2['SolO--Flux'] = weighted_bin_merge(solo_df1, 'solo', 'p', bin_list, ['H_Flux','H_Flux_'], bin_width)
+        solo_df2['SolO--Uncertainty'] = weighted_bin_merge(solo_df1, 'solo', 'p', bin_list, ['H_Uncertainty','H_Uncertainty_'], bin_width)
 
         solo_df3 = pd.DataFrame.from_dict(solo_df2)
         solo_df3.set_index('Time', inplace=True)
 
         # Resample
-        solo = solo_df3.resample(resample).mean()
+        solo = solo_df3.resample(resampling).mean()
 
         # Add to the collection
         sc_dict['Solar Orbiter - EPD/HET'] = solo
 
 
-    sc_df = pd.concat(sc_dict.keys(), axis=1, join='outer')
+    # Merge all the relevant columns into one df
+    print(sc_dict)
+    sc_df = pd.concat(sc_dict.values(), axis=1, join='outer')
 
     return sc_df
 
